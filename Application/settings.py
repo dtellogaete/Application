60c4af79bb15cdd7d6f27c104001eb84b8cd9ace
Daniel Alexander Tello Gaete <dtellogaete@gmail.com>
2019-03-17 23:38:00 -0300

    Commit 17/03

--- Application/settings.py
+++ Application/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.metriapps',
-    'apps.ventas',
+    'apps.adopcion',
+    'apps.mascota',
 ]
 
 MIDDLEWARE = [
--- apps/metriapps/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MetriappsConfig(AppConfig):
-    name = 'metriapps'
