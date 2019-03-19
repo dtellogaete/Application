dac7383b5af5357ea16d5c805f3aad728b98a4d7
Daniel Alexander Tello Gaete <dtellogaete@gmail.com>
2019-03-19 19:59:46 -0300

    19/03

--- apps/mascota/views.py
+++ apps/mascota/views.py
@@ -5,3 +5,13 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'mascota/index.html')
+
+def mascota_view(request):
+	if request.method == 'POST':
+		form = MascotaForm(request.POST)
+		if form.is_valid():
+			form.save()
+		return redirect('mascota:mascota_listar')
+	else:
+		form = MascotaForm()
+	return render(request, 'mascota/mascota_form.html', {'form':form})
